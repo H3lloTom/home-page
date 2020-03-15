2c18ee54346f3040dac0a9f531a32a92eb4d4234
yance <963945230@qq.com>
2020-03-15 23:42:49 +0800

    changehost

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Routes from './Routes';
 AV.init({
   appId: 'mdia1eUdHV2dArmqzsY0VQ3b-gzGzoHsz',
   appKey: 'RB6pTj7BTomYxy7Bkw0zPqF7',
-  serverURLs: 'http://api.xuyanqi.com'
+  serverURLs: '//api.xuyanqi.com'
 });
 
 const App = () => {
