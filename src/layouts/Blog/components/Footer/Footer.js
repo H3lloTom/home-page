9279ad5ffd793bb09f25e1b27ce2d981598a1c9b
xuyanqi <963945230@qq.com>
2020-03-20 15:59:11 +0800

    dev

--- src/layouts/Blog/components/Footer/Footer.js
+++ src/layouts/Blog/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { EuiFlexGroup, EuiFlexItem, EuiLink } from '@elastic/eui';
+import styles from './index.module.scss';
 
 const Footer = () => {
   return (
-    <EuiFlexGroup justifyContent="spaceAround">
+    <EuiFlexGroup justifyContent="spaceAround" className={styles.footer}>
       <EuiFlexItem grow={false}>
         <EuiLink href="http://www.beian.miit.gov.cn" color="text" external>
           浙ICP备19026791号-1
